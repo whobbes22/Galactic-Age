33d1fc896fe7e113717ec1215408b40e72d9ce23
Joe W <whobbes22@gmail.com>
2023-04-14 10:28:53 -0700

    new test 2

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -10,7 +10,7 @@ describe('Calculator', () =>{
   test("calculate years passing based on inputted earth years", () => {
     const ageCalc = new AgeCalculator("joe",56);
     ageCalc.YearsPassed(43);
-    expect(ageCalc.calculations).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1});
+    expect(ageCalc.calculations).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1}); 
   })
 
 });
