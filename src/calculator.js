ed3d903a4727047d514bae0986fbb33ba9bef4a0
Joe W <whobbes22@gmail.com>
2023-04-14 10:16:58 -0700

    new test 1 passed

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,14 @@
 export default class AgeCalculator{
-  constructor(earthYears){
-    this.earth = earthYears;
-    this.mercury = Math.round(earthYears /.24 * 100) / 100;
-    this.venus = Math.round(earthYears / .62 * 100) / 100;
-    this.mars = Math.round(earthYears / 1.88 * 100) / 100;
-    this.jupiter = Math.round(earthYears / 11.86 * 100) / 100;
+  constructor(name,age){
+    this.name = name;
+    this.age = age;
   }
+
+  // constructor(earthYears){
+  //   this.earth = earthYears;
+  //   this.mercury = Math.round(earthYears /.24 * 100) / 100;
+  //   this.venus = Math.round(earthYears / .62 * 100) / 100;
+  //   this.mars = Math.round(earthYears / 1.88 * 100) / 100;
+  //   this.jupiter = Math.round(earthYears / 11.86 * 100) / 100;
+  // }
 }
