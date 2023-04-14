5dc2f39299d30d8e671d2fddb053af778e1dd1ea
Joe W <whobbes22@gmail.com>
2023-04-14 11:08:38 -0700

    add 3rd test for future years

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -13,4 +13,10 @@ describe('Calculator', () =>{
     expect(ageCalc.calculations).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1}); 
   })
 
+  test("calculate years yet to have been passed", () => {
+    const ageCalc = new AgeCalculator("joe",56)
+    ageCalc.futureYears(61);
+    expect(ageCalc.calculations).toEqual({earth: 5,mercury:20.83,venus: 8.06,mars: 2.65,jupiter:0.42}); 
+  })
+
 });
