bf92d37e417addc03dcd68062f99800c37213d91
Joe W <whobbes22@gmail.com>
2023-04-14 15:52:56 -0700

    passed 6th test

--- src/calculator.js
+++ src/calculator.js
@@ -25,4 +25,12 @@ export default class AgeCalculator{
     this.calculateMayFlyAge();
     this.calculations.mayFlyAgeRatioToSun =  Math.round(4603000000 / this.calculations.mayFlyAge);
     }
+
+  calculateDogYears(years){
+    this.calculations.earth = Math.round(years * 100 / 7) / 100;
+    this.calculations.mercury = Math.round(years /.24 * 100 / 7) / 100 ;
+    this.calculations.venus = Math.round(years / .62 * 100 / 7) / 100;
+    this.calculations.mars = Math.round(years / 1.88 * 100 / 7) / 100;
+    this.calculations.jupiter = Math.round(years / 11.86 * 100 / 7) / 100;
+  }
 }
