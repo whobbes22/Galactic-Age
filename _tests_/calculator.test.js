510ad608616ff1f94f2e575c5a9837d677e98c0a
Joe W <whobbes22@gmail.com>
2023-04-14 10:27:14 -0700

    updated test 1 to hold a calculations object

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -4,12 +4,13 @@ describe('Calculator', () =>{
 
   test("should verify calculator object", () => {
     const ageCalc = new AgeCalculator("joe",514);
-    expect(ageCalc).toEqual({name:"joe",age:514});
+    expect(ageCalc).toEqual({name:"joe",age:514,calculations: {}});
   })
 
-  // test("calculate years passing based on inputted earth years", () => {
-  //   const ageCalc = new AgeCalculator(13);
-  //   expect(ageCalc).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1  });
-  // })
+  test("calculate years passing based on inputted earth years", () => {
+    const ageCalc = new AgeCalculator("joe",56);
+    ageCalc.YearsPassed(43);
+    expect(ageCalc.calculations).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1});
+  })
 
 });
