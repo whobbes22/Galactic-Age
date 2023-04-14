0a3c18dc4b3ff3b4f52f3adfc1c1a690c0d1c770
Joe W <whobbes22@gmail.com>
2023-04-14 10:15:35 -0700

    moved current test 1 to test 2 made new test 1 to put a name and age of a person into the object.

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -3,8 +3,13 @@ import AgeCalculator from "../src/calculator.js";
 describe('Calculator', () =>{
 
   test("should verify calculator object", () => {
-    const ageCalc = new AgeCalculator(13);
-    expect(ageCalc).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1  });
+    const ageCalc = new AgeCalculator("joe",514);
+    expect(ageCalc).toEqual({name:"joe",age:514});
   })
+
+  // test("calculate years passing based on inputted earth years", () => {
+  //   const ageCalc = new AgeCalculator(13);
+  //   expect(ageCalc).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1  });
+  // })
 
 });
