69de74389795bf40aa69d36a14b100fa08617c84
Joe W <whobbes22@gmail.com>
2023-04-14 10:33:03 -0700

    passed 2nd test yearsPassed

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -9,7 +9,7 @@ describe('Calculator', () =>{
 
   test("calculate years passing based on inputted earth years", () => {
     const ageCalc = new AgeCalculator("joe",56);
-    ageCalc.YearsPassed(43);
+    ageCalc.yearsPassed(43);
     expect(ageCalc.calculations).toEqual({earth: 13,mercury:54.17,venus: 20.97,mars: 6.91,jupiter:1.1}); 
   })
 
--- src/calculator.js
+++ src/calculator.js
@@ -2,14 +2,15 @@ export default class AgeCalculator{
   constructor(name,age){
     this.name = name;
     this.age = age;
-    this.calculations ={};
+    this.calculations = {};
   }
 
-  // constructor(earthYears){
-  //   this.earth = earthYears;
-  //   this.mercury = Math.round(earthYears /.24 * 100) / 100;
-  //   this.venus = Math.round(earthYears / .62 * 100) / 100;
-  //   this.mars = Math.round(earthYears / 1.88 * 100) / 100;
-  //   this.jupiter = Math.round(earthYears / 11.86 * 100) / 100;
-  // }
+  yearsPassed(years){
+    const earthYears = this.age - years;
+    this.calculations.earth = earthYears;
+    this.calculations.mercury = Math.round(earthYears /.24 * 100) / 100;
+    this.calculations.venus = Math.round(earthYears / .62 * 100) / 100;
+    this.calculations.mars = Math.round(earthYears / 1.88 * 100) / 100;
+    this.calculations.jupiter = Math.round(earthYears / 11.86 * 100) / 100;
+  }
 }
